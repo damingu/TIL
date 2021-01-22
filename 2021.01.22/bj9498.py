23bc551e32d95021122688e32bcc18b107c25ba0
damingu <daministrator1211@gmail.com>
2021-01-23 02:13:03 +0900

    Add bj-if

---
+++ 2021.01.22/bj9498.py
@@ -0,0 +1,11 @@
+scr= int(input())
+if scr >= 90 and scr <= 100:
+    print("A")
+elif scr >= 80 and scr < 90 :
+    print("B")
+elif scr >= 70 and scr < 80 :
+    print("C")
+elif scr >= 60 and scr < 70 :
+    print("D")
+else:
+    print("F")
